1ed444fcca91983d5465d79200b0f6d17a63a010
Segun Ogundipe <segunogundipe@witts-stratts.com>
2019-12-19 23:29:44 +0100

    add comment

--- part-1/main.ts
+++ part-1/main.ts
@@ -1,11 +1,21 @@
-const fuelRequiredByMass = (mass: number): number => {
+/**
+ * Returns the fuel required by a module.
+ *
+ * @param mass - A numeric mass
+ */
+function fuelRequiredByMass(mass: number): number {
   if (mass <= 0) return 0;
   const fuelRequired: number = Math.floor(mass / 3) - 2;
   return fuelRequired;
-};
+}
 
-const totalFuelRequiredByMass = (masses: number[]): number => {
+/**
+ * Returns the total fuel required by the modules in an array of modules.
+ *
+ * @param masses - An array of module masses
+ */
+function totalFuelRequiredByMass(masses: number[]): number {
   return masses.reduce((acc, current) => acc + fuelRequiredByMass(current), 0);
-};
+}
 
 export { fuelRequiredByMass, totalFuelRequiredByMass };
--- part-2/main.ts
+++ part-2/main.ts
@@ -1,14 +1,25 @@
-const fuelRequiredByFuel = (mass: number, sum: number = 0): number => {
+/**
+ * Returns the fuel required by a module's fuel.
+ *
+ * @param mass - A numeric mass.
+ * @param sum - Used to derive total fuel requirement.
+ */
+function fuelRequiredByFuel(mass: number, sum: number = 0): number {
   if (mass <= 0) return 0;
   let requiredFuel: number = Math.floor(mass / 3) - 2;
   if (requiredFuel <= 0) {
     return sum;
   }
   return fuelRequiredByFuel(requiredFuel, sum + requiredFuel);
-};
+}
 
-const totalFuelRequiredByFuel = (masses: number[]): number => {
+/**
+ * Returns the total fuel required by the fuel of the modules in an array of modules.
+ *
+ * @param masses - An array of module masses
+ */
+function totalFuelRequiredByFuel(masses: number[]): number {
   return masses.reduce((acc, current) => acc + fuelRequiredByFuel(current), 0);
-};
+}
 
 export { fuelRequiredByFuel, totalFuelRequiredByFuel };
